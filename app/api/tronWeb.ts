9ddc5a7cd8278a14c23aad7dbcd359a3bce06db0
Ameno <bravesfly@gmail.com>
2024-06-09 18:23:19 +0800

    edit tron network

--- app/api/tronWeb.ts
+++ app/api/tronWeb.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
 import TronWeb from 'tronweb';
 export const tronWeb: any = new TronWeb({
-    fullHost: 'https://api.nileex.io',
+    fullHost: 'https://api.trongrid.io',
 });
 if (typeof window !== 'undefined') {
     (window as any).tronWeb1 = tronWeb;
